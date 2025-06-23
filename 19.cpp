2c84c06b1ea099d18275bd0b8169e9bfd09c49de
Sumit <sumit.cse24@gmail.com>
2025-06-23 12:55:47 +0530

    completing the solution

--- 19.cpp
+++ 19.cpp
@@ -1,13 +1,30 @@
 /*
-Chef is playing with weights. He has an object weighing 
-W
-W units. He also has three weights each of 
-X
-,
-Y
-,
-X,Y, and 
-Z
-Z units respectively. Help him determine whether he can measure the exact weight of the object with one or more of these weights.
-
+Chef is playing with weights. He has an object weighing W units. He also has three weights each of X,Y, and Z units respectively.
+Help him determine whether he can measure the exact weight of the object with one or more of these weights.
 If it is possible to measure the weight of object with one or more of these weights, print YES, otherwise print NO.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int x,y,z,w;
+        cin>>w>>x>>y>>z;
+        if (x==w || y==w ||z==w)
+        cout<<"yes"<<endl;
+        else if(x+y+z == w)
+        cout <<"yes"<<endl;
+        else if((x+y)==w )
+        cout <<"yes"<<endl;
+        else if ((x+z)==w)
+        cout<<"yes"<<endl;
+        else if((y+z)==w)
+        cout<<"yes"<<endl;
+        else
+        cout<<"no"<<endl;
+    }
+    
+    return 0;
+}
