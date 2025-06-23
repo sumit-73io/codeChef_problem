d623a14d0669db318b7829166c84e44d7e2f6880
Sumit <sumit.cse24@gmail.com>
2025-06-23 12:05:10 +0530

    added sale season

--- 18.cpp
+++ 18.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main(){
     cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "hello world "<<endl;
+    cout << "How are you all ? "<<endl;
     return 0;
 }
