f93d831db88ccdd1e7634ac59f0ec82b7f0e8c03
Sumit <sumit.cse24@gmail.com>
2025-06-23 12:11:28 +0530

    adding new question

---
+++ 19.cpp
@@ -0,0 +1,13 @@
+/*
+Chef is playing with weights. He has an object weighing 
+W
+W units. He also has three weights each of 
+X
+,
+Y
+,
+X,Y, and 
+Z
+Z units respectively. Help him determine whether he can measure the exact weight of the object with one or more of these weights.
+
+If it is possible to measure the weight of object with one or more of these weights, print YES, otherwise print NO.*/
